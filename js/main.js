754c52a4d98fca729486a32b4d5ec7a184a2645d
abbosxon <abbosxon.oripov@yandex.ru>
2022-05-05 21:03:31 +0500

    fixed

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@ let td = document.querySelector("td");
 let age_list = document.getElementById('age-list');
 let info = document.getElementById('info-list')
 let Additionaly = document.getElementById("new-td")
-console.log(age);
+let submit = document.getElementById("submit");
 console.log(search);
+console.log(age);
 console.log(td);
 console.log(age_list);
 console.log(Additionaly);
 console.log(info);
 console.log(submit);
+
+
+// 30 , 60 , 20 , 30 , 40
+
+console.log(2 + 3 + 3 + 4 + 1 + 3 + 2);
