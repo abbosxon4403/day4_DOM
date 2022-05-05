611b587e26ccf0402b690420de1e8950d7b85133
abbosxon <abbosxon.oripov@yandex.ru>
2022-05-05 18:48:45 +0500

    add DOM

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let age = document.getElementById("age");
+let search = document.getElementById("search");
+let td = document.querySelector("td");
+let age_list = document.getElementById('age-list');
+let info = document.getElementById('info-list')
+let Additionaly = document.getElementById("new-td")
+console.log(age);
+console.log(search);
+console.log(td);
+console.log(age_list);
+console.log(Additionaly);
+console.log(info);
+console.log(submit);
